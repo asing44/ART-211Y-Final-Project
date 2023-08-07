0d3f496f82bfeca4a9b1f494a8b1e18da7a52154
asing44 <109570966+asing44@users.noreply.github.com>
2023-08-07 08:29:20 -0400

    8/7/23

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 let circle = document.getElementsByClassName("lb-background")[0];
 
-console.log(circle)
+let t1 = gsap.timeline({
+    paused: false,
+    repeat: -1,
+    repeatDelay: 0.25,
+    defaults: {
+        duration: 5,
+        ease: "power1.out"
+    }
+});
 
-alert("test")
+t1.to(circle, {
+    height: 70 + "rem",
+    width: 70 + "rem",
+    opacity: 100
+});
+
+t1.to(circle, {
+    opacity: 0,
+    borderWidth: 0.25 + "rem",
+}, "<")
